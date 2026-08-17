c4ea01d281d3fea32cfaf5e22a73649e33127eed
Gustavo Piumbini <gustavopiumbini27@gmail.com>
2026-08-17 20:39:14 -0300

    Add files via upload

---
+++ lista.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lista.h"
+
+lista *cria_lista (size_t n) {
+    lista *L;
+}
+
+L = (lista *)
---
+++ lista.h
@@ -0,0 +1,21 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+typedef struct {
+    size_t capacidade;
+    int ultimo;
+    int *elementos;
+} lista;
+
+lista* cria_lista(size_t n);
+
+void destroi_lista (lista *L);
+
+int insere_lista(lista *L, int elemento);
+
+int remover_lista(lista *L, int elemento);
+
+int buscar_lista(lista *L, int elemento);
+
+#endif
+
