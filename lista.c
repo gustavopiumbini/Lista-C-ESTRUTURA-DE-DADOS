54080fe084e51300f6ce5ef059a980428e9540f8
Gustavo Piumbini <gustavopiumbini27@gmail.com>
2026-08-18 22:02:08 +0000

    alteração lista.c

--- lista.c
+++ lista.c
@@ -4,6 +4,21 @@
 
 lista *cria_lista (size_t n) {
     lista *L;
+    L = (lista *) malloc (sizeof(lista));
+    if (L == NULL){
+        return NULL;
+    }
+
+L->elementos = (int *) malloc(n * sizeof(int));
+
+if (L->elementos == NULL){
+    free(L);
+    return NULL;
 }
 
-L = (lista *)
+L->capacidade = n;
+L->ult_pos = -1;
+
+return L;
+
+}
